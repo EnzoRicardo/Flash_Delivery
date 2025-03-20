459317f23d960d5214349a42fbce5bd12ca60a5e
EnzoRicardo <enzoricardodeoliveira@gmail.com>
2025-03-20 00:08:08 -0300

    Landing page finalizada

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import figura1 from "../assets/Blob 3.svg"
+import figura2 from "../assets/Blob 5.svg"
+import figura3 from "../assets/Blob 14.svg"
+import figura4 from "../assets/Lightning 2.svg"
+
 
 const Hero = () => {
   return (
     <section className="textLP">
-      <img src="../src/assets/F1.png" alt="Imagem Geometrica" className="background-image" />
+      <img src={figura1}  className="figure1" />
+      <img src={figura2}  className="figure2" />
+      <img src={figura3}  className="figure3" />
+      <img src={figura4}  className="figure4" />
     <h1>
       <div className="left-align">SUA ENTREGA</div>
       <span>NA <span className="yellow">VELOCIDADE</span> DA SUA</span> <span className="red">FOME.</span>
