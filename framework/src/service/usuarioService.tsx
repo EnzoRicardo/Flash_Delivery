b909b6f1f18feed6861cd16aeba619202ecc0028
EnzoRicardo <enzoricardodeoliveira@gmail.com>
2025-05-28 19:29:51 -0300

    sistema de login funcionando

--- framework/src/service/usuarioService.tsx
+++ framework/src/service/usuarioService.tsx
@@ -1,43 +1,40 @@
-import React from 'react'
-    
-const salvar = async(usuario:any) => {
-return await fetch(`http://localhost:8080/api/usuario`,{
-        method: "POST",
-        headers: {
-            "Content-type": "application/json",
-        },
-        body: JSON.stringify(usuario),
-    })
-}
+const salvar = async (usuario: any) => {
+  return await fetch(`http://localhost:8080/api/usuario`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(usuario),
+  });
+};
 
-const login = async (email: string, senha: string, navigate: any) => {
-    const response = await fetch(`http://localhost:8080/api/login`, {
-        method: "POST", 
-        headers: {
-            "Content-type": "application/json",
-        },
-        body: JSON.stringify({ email, senha }),
-    });
+const login = async (email: string, senha: string) => {
+  const response = await fetch(`http://localhost:8080/api/login`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ email, senha }),
+  });
 
+  const contentType = response.headers.get("content-type");
+
+  if (contentType && contentType.includes("application/json")) {
     const data = await response.json();
-    console.log("Resposta do servidor:", data);
-
-    if (response.ok) {
-        console.log("Login bem-sucedido!");
-        console.log("Token recebido:", data.token);
-        localStorage.setItem("userToken", data.token);
-        alert("Login bem-sucedido!");
-        navigate("/");
-        return data;
-    } else {
-        console.log("Erro no login:", data.message);
-        alert(data.message);
-    }
+    return {
+        ok: response.ok,
+        status: response.status,
+        data,
+    };
+  } else {
+    const text = await response.text();
+    throw new Error("Resposta esperada da API" + text.slice(0, 100));
+  }
 };
 
 const usuarioService = {
-    salvar,
-    login
+  salvar,
+  login,
 };
 
 export default usuarioService;
